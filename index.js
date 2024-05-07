a51f490340830db601abfc7a0329643fb23d3d04
roughnut <jeremy@writever.com.au>
2024-05-07 14:02:17 +1000

    Add completed files to repo

--- index.js
+++ index.js
@@ -1,13 +1,82 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'Please provide a brief description of your project.',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'What are the installation instructions for your project?',
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: 'What are the usage instructions for your project?',
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: 'What are the contribution guidelines for your project?',
+        name: 'contribution'
+    },
+    {
+        type: 'input',
+        message: 'What are the test instructions for your project?',
+        name: 'tests'
+    },
+    {
+        type: 'list',
+        message: 'What license would you like to use?',
+        name: 'license',
+        choices: ['MIT', 'GNU GPLv3', 'Apache 2.0', 'ISC', 'None']
+    },
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'github'
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email'
+    }
+];
 
-// TODO: Create a function to initialize app
-function init() {}
+// Initialise the app 
+function init() {
+    console.log('Welcome to the README.md generator. The quick and easy way to write less markdown and more code!');
+    inquirer.prompt(questions)
+    .then((answers) => {
+        console.log(answers);
 
-// Function call to initialize app
+        const markdown = generateMarkdown(answers);
+
+        // Create a directory called output and write the README.md file to it - it was overwriting my README.md file in the challenge directory
+        const outputDir = path.join(__dirname, 'output');
+        const outputPath = path.join(outputDir, 'README.md');
+        // Check if the directory exists, if not create it
+        if (!fs.existsSync(outputDir)) {
+            fs.mkdirSync(outputDir);
+        }
+        fs.writeFile(outputPath, markdown, 'utf-8', (err) => {
+            if (err) throw err;
+            console.log(`Nice one! Your README.md is created in the directory ${outputDir}!`);
+        });
+    });
+}
+
+// Start this puppy up!
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,89 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case "MIT":
+      return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](${renderLicenseLink(
+        license
+      )})`;
+    case "GNU GPLv3":
+      return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](${renderLicenseLink(
+        license
+      )})`;
+    case "Apache 2.0":
+      return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](${renderLicenseLink(
+        license
+      )})`;
+    case "ISC":
+      return `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](${renderLicenseLink(
+        license
+      )})`;
+    case "None":
+      return "";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case "MIT":
+      return "https://opensource.org/licenses/MIT";
+    case "GNU GPLv3":
+      return "https://www.gnu.org/licenses/gpl-3.0";
+    case "Apache 2.0":
+      return "https://opensource.org/licenses/Apache-2.0";
+    case "ISC":
+      return "https://opensource.org/licenses/ISC";
+    case "None":
+      return "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === "None") {
+    return "No license";
+  }
+  return `This project is licensed under the ${license} license.`;
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(answers) {
+  return `
+  # ${answers.title} ${renderLicenseBadge(answers.license)}
+  ## Description
+  ${answers.description}
 
-`;
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Testing](#tests)
+  - [License](#license)
+  - [Questions](#questions)
+
+  ## Installation
+  ${answers.installation}
+
+  ## Usage
+  ${answers.usage}
+
+  ## Contribution
+  ${answers.contribution}
+
+  ## Testing
+  ${answers.tests}
+
+  ## License
+  ${renderLicenseSection(answers.license)}
+
+  ## Questions
+  - GitHub: [${answers.github}](https://github.com/${answers.github})
+  - Email: [${answers.email}](mailto:${answers.email})
+  `;
+
 }
 
 module.exports = generateMarkdown;
